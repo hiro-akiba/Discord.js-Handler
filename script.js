84a3343187f85b29932edb5d1602c8b7151cbd39
Zerio <43386412+ZerioDev@users.noreply.github.com>
2019-11-24 17:04:11 +0100

    Script to be modified as needed

---
+++ script.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message) => {
+
+//script
+
+}
+module.exports.config = {
+    name: "NAME", //name
+    aliases: ["ALIASES"] //aliases
+}

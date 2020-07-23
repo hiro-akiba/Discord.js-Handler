47bb38ba733444677bbbd4f132376a96287b220b
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-07-23 09:27:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ client.login("TOKEN");
 
 client.on("ready", async () => {
 
-    console.log(`[✅] Bot on !`);
+    console.log(`(✅)Bot on !`);
 });
 
 
 client.on("message", async message => {
+
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0].toLowerCase();
   let args = messageArray.slice(1);
-
-  
   let prefix = "PREFIX"
 
   if (!message.content.startsWith(prefix)) return;
@@ -22,22 +21,31 @@ client.on("message", async message => {
     client.commands.get(cmd.slice(prefix.length)) ||
     client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
   if (commandfile) commandfile.run(client, message, args);
+
 });
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
+
   let jsfile = files.filter(f => f.split(".").pop() === "js");
+  
   if (jsfile.length <= 0) {
     return console.log("[LOGS] Couldn't Find Commands !");
   }
+
   jsfile.forEach((f, i) => {
+
     console.log(`[📫] Command loaded ${f}`);
+
     let pull = require(`./commands/${f}`);
     client.commands.set(pull.config.name, pull);
+
     pull.config.aliases.forEach(alias => {
+
       client.aliases.set(alias, pull.config.name);
+   
     });
   });
 });
